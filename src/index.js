f06091f69c92212d911cacf85e1586c16830546c
franrodriguez1993 <francojmprogramacion@gmail.com>
2023-09-01 16:35:11 -0300

    proveedores

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const { vehiculoRoute } = require("./routes/vehiculo.routes");
+const { proveedorRoutes } = require("./routes/proveedor.routes");
 
 const app = express();
 const PORT = process.env.PORT;
@@ -9,6 +10,7 @@ app.use(express.json());
 
 //rutas:
 app.use("/vehiculo", vehiculoRoute);
+app.use("/proveedor", proveedorRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on PORT: ${PORT}`);
